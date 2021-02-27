6064204d2b227d84686acda6c242b1f5542fa396
Kat Leight <leightkt@gmail.com>
2021-02-27 14:15:41 -0700

    added create and destroy routes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,4 +4,20 @@ class TodosController < ApplicationController
         @todos = Todo.all
         render json: @todos
     end
+
+    def create
+        @todo = Todo.create(
+            title: params[:title],
+            content: params[:content],
+            urgent: params[:urgent],
+            done: params[:done]
+        )
+        render json: @todo, status: :created
+    end
+
+    def destroy
+        @todo = Todo.find(params[:id])
+        @todo.destroy
+        render status: :no_content
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :todos, only: :index
+  resources :todos, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
