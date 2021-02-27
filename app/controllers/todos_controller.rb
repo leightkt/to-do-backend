419227ec4df42d99377baba708f346953ecef93d
Kat Leight <leightkt@gmail.com>
2021-02-27 14:32:27 -0700

    added update route and strong params

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+    before_action :find_todo, only: [:update, :destroy]
 
     def index
         @todos = Todo.all
@@ -6,18 +7,27 @@ class TodosController < ApplicationController
     end
 
     def create
-        @todo = Todo.create(
-            title: params[:title],
-            content: params[:content],
-            urgent: params[:urgent],
-            done: params[:done]
-        )
+        @todo = Todo.create(todo_params)
         render json: @todo, status: :created
     end
 
+    def update
+        @todo.update(todo_params)
+        render json: @todo, status: :ok
+    end
+
     def destroy
-        @todo = Todo.find(params[:id])
         @todo.destroy
         render status: :no_content
+    end
+
+    private
+
+    def find_todo
+        @todo = Todo.find(params[:id])
+    end
+
+    def todo_params
+        params.require(:todo).permit(:title, :content, :urgent, :done)
     end
 end
